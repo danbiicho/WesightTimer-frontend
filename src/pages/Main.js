77ada6b8928e42eb50bdbfbb5c76d5280272511b
Sun ju Lee <yohekisa@gmail.com>
2020-05-10 16:30:06 +0900

    modified git ignore

---
+++ src/pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from '../components/Nav';
+import styled from 'styled-components';
+
+const Main = () => {
+    return (
+        <>
+        <Nav />
+        <section>
+            Wesight Timer Main
+        </section>
+        </>
+    )
+}
+
+
+export default Main;
